228f88fc7cd4cad4c77873260202f454e2a6ec27
Omarbit26 <155489881+Omarbit26@users.noreply.github.com>
2024-07-23 22:49:37 -0500

    first commit

---
+++ src/test/java/com/omar/ec/FirstApiRestApplicationTests.java
@@ -0,0 +1,20 @@
+package com.omar.ec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstApiRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
+
+
+
+
+
+
+
